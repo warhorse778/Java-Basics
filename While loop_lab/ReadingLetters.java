f77e05404392cd4ead8cd8b932a6ecf0bf754307
Mihail Stoyanov <michaell775533@gmail.com>
2022-04-01 11:26:16 +0300

    Update

---
+++ While loop_lab/ReadingLetters.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ReadingLetters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        while (!input.equals("Stop")){
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+    }
+}
