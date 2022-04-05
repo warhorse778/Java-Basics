f450375ee1cd9fc28ad31703af668c306b1d02dc
Mihail Stoyanov <michaell775533@gmail.com>
2022-04-05 13:00:19 +0300

    Fibonacciii

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+
+    class fibonacci
+    {
+        static long fib(long n)
+        {
+            if (n <= 1)
+                return n;
+            return fib(n-1) + fib(n-2);
+        }
+
+        public static void main (String args[])
+        {
+            long n = 9;
+            System.out.println(fib(n));
+        }
+    }
+
