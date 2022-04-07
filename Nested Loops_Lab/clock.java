ffd0018b53ed4e295f8c817f139729eca4f69a51
Mihail Stoyanov <michaell775533@gmail.com>
2022-04-07 23:55:17 +0300

    update

--- Nested Loops_Lab/clock.java
+++ Nested Loops_Lab/clock.java
@@ -5,7 +5,7 @@ public class clock {
         Scanner sc = new Scanner(System.in);
 
         for (int h=0;h <=23;h++) {
-            for (int m = 0; m <= 60; m++) {
+            for (int m = 0; m <= 59; m++) {
                 System.out.printf("%d:%d%n", h, m);
             }
         }
